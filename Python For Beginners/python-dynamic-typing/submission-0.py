f911c3d6edeb13e2c8174f564f367d03aa8ced11
Shegzi <66688833+shegz101@users.noreply.github.com>
2026-05-18 20:03:12 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 40
+print(type(variable))
+variable = 2.6
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Olusegun"
+print(type(variable))
+variable = [3, 6, 8]
+print(type(variable))
