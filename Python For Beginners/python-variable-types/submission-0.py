785ec23ee22e7d8ef4a6509226f27e1ded0b6ee2
Shegzi <66688833+shegz101@users.noreply.github.com>
2026-05-18 19:58:10 +0100

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 40
+float_type = 2.5
+bool_type = True
+string_type = "Segun"
+list_type = [1, 2, 3]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
