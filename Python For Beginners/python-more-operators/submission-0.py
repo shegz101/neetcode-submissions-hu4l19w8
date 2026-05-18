5aae8af59d4d032f17e3ba390b8a134529a3743a
Shegzi <66688833+shegz101@users.noreply.github.com>
2026-05-18 21:10:21 +0100

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,10 @@
+a, b, c = 2, 8, 5
+first_operation = a * b // c
+print(first_operation)
+modulus_operation = a % b
+second_operation = a * b %c
+print(second_operation)
+third_operation = a ** b
+print(third_operation)
+fourth_operation = b ** c
+print(fourth_operation)
