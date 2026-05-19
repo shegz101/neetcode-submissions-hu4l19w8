7269d9872d4560bcb78248b91f196c44b62d92a6
Shegzi <66688833+shegz101@users.noreply.github.com>
2026-05-19 15:34:50 +0100

    Add: python-strings-are-immutable - submission-0

---
+++ Python For Beginners/python-strings-are-immutable/submission-0.py
@@ -0,0 +1,12 @@
+def remove_fourth_character(word: str) -> str:
+    before_fourth = word[:3]
+    after_fourth = word[4:]
+
+    new_string = before_fourth + after_fourth
+
+    return new_string
+
+
+# do not modify below this line
+print(remove_fourth_character("NeetCode"))
+print(remove_fourth_character("Hello"))
