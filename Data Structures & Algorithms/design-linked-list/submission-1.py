564383839cc24cfcf1d7b33500dc48e8263d02ec
Shegzi <66688833+shegz101@users.noreply.github.com>
2026-05-31 23:24:58 +0100

    Add: design-linked-list - submission-1

---
+++ Data Structures & Algorithms/design-linked-list/submission-1.py
@@ -0,0 +1,65 @@
+class DoublyNode:
+    def __init__(self, val, next=None, prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = DoublyNode(-1)
+        self.tail = DoublyNode(-1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+        curNode = self.head.next
+        while index > 0:
+            curNode = curNode.next
+            index -= 1
+        # for _ in range(index):
+        #     curNode = curNode.next
+        return curNode.val
+
+    def addAtHead(self, val: int) -> None:
+        self.addAtIndex(0, val)
+
+    def addAtTail(self, val: int) -> None:
+        self.addAtIndex(self.size, val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index > self.size:
+            return
+        if index < 0:
+            index = 0
+        curNode = self.head.next
+        for _ in range(index):
+            curNode = curNode.next
+        newNode = DoublyNode(val)
+        newNode.prev = curNode.prev
+        newNode.next = curNode
+        curNode.prev = newNode
+        newNode.prev.next = newNode
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return -1
+        curNode = self.head.next
+        for _ in range(index):
+            curNode = curNode.next
+        curNode.prev.next = curNode.next
+        curNode.next.prev = curNode.prev
+        self.size -= 1
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
