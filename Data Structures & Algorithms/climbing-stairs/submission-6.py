f2e9be5d993fdcc91b40a9cf6b65458a75b56358
Shegzi <66688833+shegz101@users.noreply.github.com>
2026-06-10 09:38:53 +0100

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,18 @@
+from functools import lru_cache
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            if i >= n:
+                return i == n
+            # if i > n:
+            #     return 0
+            # elif i == n:
+            #     return 1
+            
+            memo[i] = dfs(i + 1) + dfs(i + 2)
+            return memo[i]
+        
+        return dfs(0)
